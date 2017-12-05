932582ba9a4639016759ec21361b53ce45fb4655
koulp <vkoulp@gmail.com>
2017-12-05 19:55:59 +0200

    2nd

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class Numbers {
@@ -9,15 +10,14 @@ public class Numbers {
         String number = in.nextLine();
         number = number.replaceAll("\\s+", "");
 
-        //try convert string to int to see if it contains only digits else scan new number
+        //try convert string to int to see if it contains only digits
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
-            System.out.println("---Your Number must contain digits only");
+            /*System.out.println("---Your Number must contain digits only");
             number = in.nextLine();
-            number = number.replaceAll("\\s+", "");
+            number = number.replaceAll("\\s+", "");*/
         }
-
         in.close();
 
         //if number is validated return 1 else 0
@@ -26,7 +26,9 @@ public class Numbers {
         else
             System.out.println("---Your Number is: " + number + " [phone number:INVALID]");
 
+        //ambiguities(number);
     }
+
 
     static int validation(String number) {
         String[] number2 = number.split("");
@@ -44,9 +46,49 @@ public class Numbers {
 
                 && (number2[4].equals("2")
                 || (number2[4].equals("6") && number2[5].equals("9")))
-            )
+                )
             return 1;
 
         return 0;
     }
+
+    /*static void ambiguities(String number) {
+        String[] valid = number.split("");
+        int [] valid2= new int[valid.length];
+
+        //converting String array to Int array
+        for (int i = 0; i < valid.length; i++) {
+            valid2[i]= Integer.parseInt(valid[i]);
+        }
+        //Adding ambiguities
+        long num = 0;
+
+        for (int a : valid2) {
+            if (a < 0) throw new IllegalArgumentException();
+            int i = a;
+            do {
+                num *= 10;
+                i /= 10;
+            } while (i != 0);
+            num += a;
+        }
+                System.out.println("Interpretation 1: " + num);
+
+        for (int i = 0; i < valid2.length; i++) {
+            int digitlength = valid[i].length();
+
+            if(digitlength == 2) {
+                for (int a : valid2) {
+                    if (a < 0) throw new IllegalArgumentException();
+                    int i2 = a;
+                    do {
+                        //num *= 10;
+                        i2 /= 10;
+                    } while (i2 != 0);
+                    num += a;
+                }
+                System.out.println("Interpretation : " + num);
+            }
+        }
+    }*/
 }
